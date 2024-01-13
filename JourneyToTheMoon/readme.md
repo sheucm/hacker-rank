30da8f634eb2b8a9ba62c796f3728c0eb4f0f546
Eric Hsu <st0502123@gmail.com>
2024-01-13 16:09:21 +0800

    Journey to the Moon

---
+++ JourneyToTheMoon/readme.md
@@ -0,0 +1,20 @@
+# Problem
+https://www.hackerrank.com/challenges/journey-to-the-moon/problem
+
+
+# Skill
+1. BFS (DFS)
+1. DP
+
+# Challenges
+1. A bit tricky at the end of collecting total combinations.
+
+# Reference
+1. https://www.youtube.com/watch?v=dP1Auzs1yJo
+
+
+# Run
+```
+export OUTPUT_PATH=./output.txt
+python sol.py < ./test_cases/1/input.txt
+```
---
+++ JourneyToTheMoon/sol.py
@@ -0,0 +1,89 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'journeyToMoon' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY astronaut
+#
+
+def journeyToMoon(n, astronaut):
+    
+    # Write your code here
+    graph = [set() for _ in range(n)]
+    for p1, p2 in astronaut:
+        graph[p1].add(p2)
+        graph[p2].add(p1)
+        
+        
+    vis = [False] * n
+    
+    group_cnts = []
+    for p in range(n):
+        if vis[p]:
+            continue
+            
+        group_cnts.append(1)
+        
+        vis[p] = True
+        
+        q = [p]
+        while q:
+            pop_p = q.pop(0)
+            for ch in graph[pop_p]:
+                if vis[ch]:
+                    continue
+                
+                vis[ch] = True
+                group_cnts[-1] += 1
+                q.append(ch)
+    
+
+    
+    ################ Use this, will cause Timeout ###########
+    # ans = 0
+    # for i in range(len(group_cnts)):
+    #     for j in range(i+1, len(group_cnts)):
+    #         ans += group_cnts[i] * group_cnts[j]
+    #########################################################
+    
+    ans = 0
+    _sum = group_cnts[0]
+    for i in range(1, len(group_cnts)):
+        ans += _sum * group_cnts[i]
+        _sum += group_cnts[i]
+        
+    return ans
+
+    
+    
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    p = int(first_multiple_input[1])
+
+    astronaut = []
+
+    for _ in range(p):
+        astronaut.append(list(map(int, input().rstrip().split())))
+
+    result = journeyToMoon(n, astronaut)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
