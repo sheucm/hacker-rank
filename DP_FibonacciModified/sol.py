c1237c973e91797a055d4f1b9bba1f887f843766
Eric Hsu <st0502123@gmail.com>
2024-01-28 09:25:29 +0800

    update code for "Fibonacci Modified"

--- DP_FibonacciModified/sol.py
+++ DP_FibonacciModified/sol.py
@@ -6,7 +6,7 @@ import random
 import re
 import sys
 sys.setrecursionlimit(10000000)
-sys.set_int_max_str_digits(50000)
+sys.set_int_max_str_digits(500000)
 #
 # Complete the 'fibonacciModified' function below.
 #
@@ -18,10 +18,19 @@ sys.set_int_max_str_digits(50000)
 #
 
 def fibonacciModified(t1, t2, n):
-    # Write your code here
-    if n == 3:
-        return t1 + t2 ** 2
-    return fibonacciModified(t2, t1 + t2**2, n-1)
+    
+    ### Solution1: DP
+    DP = [0] * (n+1)
+    DP[1] = t1
+    DP[2] = t2
+    for i in range(3, n+1):
+        DP[i] = DP[i-2] + DP[i-1] ** 2
+    return DP[n]
+    
+    ### Solution2: Recursive
+    # if n == 3:
+    #     return t1 + t2 ** 2
+    # return fibonacciModified(t2, t1 + t2**2, n-1)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
