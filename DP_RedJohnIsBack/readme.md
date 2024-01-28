c901803bcebc5a6976af9a23c009facbdd210684
Eric Hsu <st0502123@gmail.com>
2024-01-28 15:38:30 +0800

    Red John is Back

---
+++ DP_RedJohnIsBack/readme.md
@@ -0,0 +1,21 @@
+
+# Problem
+https://www.hackerrank.com/challenges/red-john-is-back/problem
+
+# Skill
+(empty)
+
+# Challenges
+(empty)
+
+# Solving Mindset
+(empty)
+
+# Reference
+- https://www.youtube.com/watch?v=g7o3bJOPfWE
+
+# Run
+```
+export OUTPUT_PATH=./output.txt
+python sol.py < ./test_cases/1/input.txt
+```
---
+++ DP_RedJohnIsBack/sol.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'redJohn' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+import math
+def get_ways(n):
+    if n <= 3:
+        return 1
+    if n == 4:
+        return 2
+    return get_ways(n-1) + get_ways(n-4)
+
+def get_prime_count(n):
+    primes = [True] * (n+1)
+    primes[0], primes[1] = False, False
+    for i in range(2, int(math.sqrt(n))+1):
+        if primes[i]:
+            for j in range(i*i, n+1, i):
+                primes[j] = False
+    return sum([1 for is_p in primes if is_p])
+
+def redJohn(n):
+    ways = get_ways(n)
+    p = get_prime_count(ways)
+    return p
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = redJohn(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
