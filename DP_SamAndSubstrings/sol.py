fed84da1afdcade6e060318d6b648a6d116751e5
Eric Hsu <st0502123@gmail.com>
2024-01-29 17:41:45 +0800

    Add comment on SamAndSubstrings prob.

--- DP_SamAndSubstrings/sol.py
+++ DP_SamAndSubstrings/sol.py
@@ -16,10 +16,14 @@ MOD = 10**9 + 7
 #
 def substrings(n):
     ### Represent the sum of all combination with n[i] within n[:i+1] 
+    ### For example:  n = "123", then DP1[2] (index=2) = 3 + 23 + 123 = 149
+    ### 3 + 23 + 123 = (12 + 2) + 3 * 3 = DP1[1] + n[2] * (2+1) = DP1[i-1] + n[i] * (i + 1)
     DP1 = [0] * len(n)
     DP1[0] = int(n[0])
     
     ### Represent the sum of all combination within n[:i+1] 
+    ### For example:  n = "123", then DP2[2] (index=2) = 1 + 2 + 3 + 12 + 23 + 123 = 164
+    ### 1 + 2 + 3 + 12 + 23 + 123 = (3 + 23 + 123) + (1 + 2 + 12) = DP1[2] + DP2[1] = DP1[i] + DP2[i-1]
     DP2 = [0] * len(n)
     DP2[0] = int(n[0])
     
